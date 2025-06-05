94f4a330f9486d08959f73e129fdf9c54c67f82c
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-05 14:50:43 +0700

    Tambahan

--- Stack.cpp
+++ Stack.cpp
@@ -28,10 +28,10 @@ class Stack
     // push operation: Insert an element onto the top of the stack
     int push (int value)
     {
-        Node*newNode = new Node();
-        newNode->data = value;
-        newNode->next = top;
-        top = newNode;
+        Node*newNode = new Node(); //1.Alocote memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top;//3. Set the next pointer of the new node to the current
+        top = newNode;//4. Update the top pointer to the new node
         cout << "push value:" << value << endl;
         return value; 
     }
