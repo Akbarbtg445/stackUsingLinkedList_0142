1ea271e4ae74c23b22c1f7458a699cb908daa9c4
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-05 14:43:30 +0700

    Membuat Stack Class

---
+++ Stack.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node*next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+//stack class
+class Stack
+{
+    private:
+    Node*top;
+
+    public:
+    Stack()
+    {
+        top = NULL;
+    }
+};
